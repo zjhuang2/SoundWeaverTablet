30619a5c8d5b62be2fcc732c85a5ac9369259beb
Jeremy Huang <jeremy.zq.huang@gmail.com>
2024-07-14 12:47:28 -0400

    Initial Commit

---
+++ SoundWeaverTablet/SoundWeaverTabletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundWeaverTabletApp.swift
+//  SoundWeaverTablet
+//
+//  Created by Jeremy Huang on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundWeaverTabletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
